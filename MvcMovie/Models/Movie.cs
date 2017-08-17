c0adc1690094fd8dfbb8117f4b7e2a5f16264511
义庆 石 <szsyq123@163.com>
2017-08-17 16:41:22 +0800

    8.17 by steven

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,14 +10,22 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60,MinimumLength =3)]
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
-        [Range(typeof(decimal),"0.0000","10000.0000")]
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString ="{0:c}")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
     public class MovieDBContext:DbContext
